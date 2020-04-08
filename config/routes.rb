0a3081f04ee71840f63013dac6c40f2a34a738f6
Ben Hsieh <bqh5026@gmail.com>
2020-04-08 12:10:40 -0400

    Remove turbolinks from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  # get 'dashboard/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root to: 'dashboard#index'
+# root to: 'dashboard#index'
+namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :new, :index, :show]
+    resource :session, only: [:new, :create, :destroy]
+end
 
-resources :users, only: [:create, :new, :index, :show]
-
-resource :session, only: [:new, :create, :destroy]
-
+  root "static_pages#root"
 end
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 
 module.exports = {
-  entry: "./frontend/entry.jsx",
+  entry: "./frontend/roberthood.jsx",
   output: {
     filename: "./bundle.js",
   },
