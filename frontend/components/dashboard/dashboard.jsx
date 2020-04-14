ff37822d6b7e1c7e561a27583d06e10a8b64353c
Ben Hsieh <bqh5026@gmail.com>
2020-04-14 15:05:40 -0400

    Add icons to menu items

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -11,7 +11,6 @@ export default ({ currentUser, logout }) => {
   console.log("currentUser", currentUser); 
 
   const [news, setNews] = useState([]);
-  console.log("currentUser", currentUser);
   useEffect(() => {
     if (news.length < 1) {
       $.ajax('/api/news/new').done(res => {
@@ -21,8 +20,6 @@ export default ({ currentUser, logout }) => {
   });
 
   
-
-
    return (
      <div>
        <div>
@@ -48,38 +45,49 @@ export default ({ currentUser, logout }) => {
            <Link className="nav-menu-item" to="#">Messages</Link>
            <div className="dropdown">
              <button className="nav-menu-item dropdown">Account</button>
-             <ul>
+             <ul className='dropdown-menu'>
               <li>
-                 <Link className="dropdown-menu-item" to="#">Robinhood Gold</Link>
+                 <img className="rg-logo" src={roberthoodGoldURL} />
+                 <Link className="gold" to="#">Roberthood Gold</Link>
               </li>
               <li>
+                 <i className="fas fa-gift"></i>
                  <Link className="dropdown-menu-item" to="#">Free Stock</Link>
               </li>
               <li>
+                 <i className="fas fa-briefcase"></i>
                  <Link className="dropdown-menu-item" to="#">Account</Link>
               </li>
               <li>
+                 <i className="fas fa-university"></i>
                  <Link className="dropdown-menu-item" to="#">Banking</Link>
               </li>
               <li>
+                 <i className="fas fa-history"></i>
                  <Link className="dropdown-menu-item" to="#">History</Link>
               </li>
               <li>
+                 <i className="fas fa-file-alt"></i>
                  <Link className="dropdown-menu-item" to="#">Documents</Link>
               </li>
               <li>
+                 <i className="fas fa-cog"></i>
                  <Link className="dropdown-menu-item" to="#">Settings</Link>
               </li>
               <li>
+                 <i className="far fa-life-ring"></i>
                  <Link className="dropdown-menu-item" to="#">Help Center</Link>
               </li>
               <li>
+                 <i className="fas fa-comment-dots"></i>
                  <Link className="dropdown-menu-item" to="#">Contact Us</Link>
               </li>
               <li>
+                 <i className="far fa-file-pdf"></i>
                  <Link className="dropdown-menu-item" to="#">Disclosure</Link>
               </li>
               <li>
+                 <i className="fas fa-sign-out-alt"></i>
                  <Link className="dropdown-menu-item" to="#">Log Out</Link>
               </li>
              </ul>
@@ -107,13 +115,16 @@ export default ({ currentUser, logout }) => {
            </div>
 
            <div className="news">
-             <h4>News</h4>
+              <h4>News</h4>
              <ul>
-               {news.map((item) => {
-                 return (<li>{item.title}</li>)
+               {news.map((item, idx) => {
+                 return (<li key={idx}>
+                  <i className="fas fa-bolt"></i>
+                  {item.title}
+                  </li>)
                })}
-             </ul>
-             <span className="show-more-news">Show More</span>
+             </ul> 
+             <span className="show-more-news">Show More</span>  
            </div>
 
            <div className="footer">
@@ -144,24 +155,4 @@ export default ({ currentUser, logout }) => {
 // </header>
 
 
-// import React from 'react'; 
-// import {Link} from 'react-router-dom'; 
 
-// class Dashboard extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   componentDidMount() {
-//     this.props.fetchUser(); 
-//   }
-//   render() {
-//     return (
-//       <div>
-//       <h1>Hello</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Dashboard; 
