531f0f78e5ca76f064ba50b82375acc086311a09
Ben Hsieh <bqh5026@gmail.com>
2020-04-15 09:27:36 -0400

    Clean up some extra codes that were used for testing

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -4,11 +4,6 @@ class Api::NewsController < ApplicationController
 
 
 def new
-    # result = URI.open("https://newsapi.org/v2/top-headlines?country=us&apiKey=eb142e3a8c1b4af9a849e5ee78714783") {|f|
-    #   f.each_line {|line| p line}
-    # }
-    # p result
-    # render result.read
 
     url = URI.parse('http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=eb142e3a8c1b4af9a849e5ee78714783')
     req = Net::HTTP::Get.new(url.to_s)
