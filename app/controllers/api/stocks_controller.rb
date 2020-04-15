7fb7582c5b4fd3182d8323b20faf03610d636383
Ben Hsieh <bqh5026@gmail.com>
2020-04-15 12:33:51 -0400

    Edit stocks controller

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -3,19 +3,19 @@ class Api::StocksController < ApplicationController
     def show
 
           symbol = params[:id]
-        #   url = URI.parse('http://cloud.iexapis.com/stable/tops?token=sk_fdb0342d026443a28057ef26a4c60a23&symbols='+symbol)
-        #   req = Net::HTTP::Get.new(url.to_s)
-        #   res = Net::HTTP.start(url.host, url.port) {|http|
-        #   http.request(req)
-        #     }
+          url = URI.parse('https://cloud.iexapis.com/stable/tops?token=sk_fdb0342d026443a28057ef26a4c60a23&symbols='+symbol)
+          req = Net::HTTP::Get.new(url.to_s)
+          res = Net::HTTP.start(url.host, url.port) {|http|
+          http.request(req)
+            }
 
-        domain='https://cloud.iexapis.com/stable/tops?token=sk_fdb0342d026443a28057ef26a4c60a23&symbols='+symbol
-        url = URI.parse(domain)
-        req = Net::HTTP::Get.new(url.request_uri)
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = (url.scheme == "https")
-        response = http.request(req)
-        render :json => res.body 
+        # domain='https://cloud.iexapis.com/stable/tops?token=sk_fdb0342d026443a28057ef26a4c60a23&symbols='+symbol
+        # url = URI.parse(domain)
+        # req = Net::HTTP::Get.new(url.request_uri)
+        # http = Net::HTTP.new(url.host, url.port)
+        # http.use_ssl = (url.scheme == "https")
+        # response = http.request(req)
+        # render :json => res.body 
 
     end
 
