b20e7018f8c09bcdd8833ee8f416031dafbc7db2
Ben Hsieh <bqh5026@gmail.com>
2020-06-02 12:31:01 -0400

    Fix autologin issue with login button. Add handledemo user function

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -10,6 +10,7 @@ class Login extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDemo = this.handleDemo.bind(this);
   }
 
   handleInput(type) {
@@ -24,6 +25,17 @@ class Login extends React.Component {
       .then(() => this.props.history.push('/dashboard'));
   }
 
+  handleDemo(e) {
+    e.preventDefault();
+    this.setState({
+      username: 'bqh5026',
+      password: 'password'
+    })
+
+    this.props.login(this.state)
+      .then(() => this.props.history.push('/dashboard'));
+  }
+
   render() {
     // console.log(this.props);
     return (
@@ -31,83 +43,82 @@ class Login extends React.Component {
         <div className="sign-in-container">
           <div className="image-container"></div>
 
-          <div className="sign-in-form">
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br className="break" />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <form className="login-form">
-              <h1 className="sign-in-header">Welcome to Roberthood</h1>
-              <span className="label">Username</span>
-              <br />
-              <br />
-              <input
-                className="input"
-                type="text"
-                value={this.state.username}
-                onChange={this.handleInput("username")}
-              />
-              <br />
-              <br />
-              <span className="label">Password</span>
-              <br />
-              <br />
-              <input
-                className="input"
-                type="password"
-                value={this.state.password}
-                onChange={this.handleInput("password")}
-              />
-              <br />
-              <br />
-              <Link to="/signup">
-                <span className="retrieval">
-                  Sign up for a new account if not already have one
-                </span>
-              </Link>
-              <br />
-              <br />
-              <button className="sign-in-btn" onClick={this.handleSubmit}>
-                Sign In
-              </button>
-              <br />
-              <br />
-            </form>
-            <div className="demo-user">
-              <form>
-                <input
-                  type="hidden"
-                  value={(this.state.username = "bqh5026")}
-                />
-                <input
-                  type="hidden"
-                  value={(this.state.password = "password")}
-                />
-                <button className="demo-user-btn" onClick={this.handleSubmit}>
-                  Demo User
-                </button>
-              </form>
-            </div>
+          <div className="forms">
+              <div className="sign-in-form">
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br className="break" />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <form className="login-form">
+                  <h1 className="sign-in-header">Welcome to Roberthood</h1>
+                  <span className="label">Username</span>
+                  <br />
+                  <br />
+                  <input
+                    className="input"
+                    type="text"
+                    value={this.state.username}
+                    onChange={this.handleInput("username")}
+                  />
+                  <br />
+                  <br />
+                  <span className="label">Password</span>
+                  <br />
+                  <br />
+                  <input
+                    className="input"
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.handleInput("password")}
+                  />
+                  <br />
+                  <br />
+                  <Link to="/signup">
+                    <span className="signup_link">
+                      Sign up for a new account if not already have one
+                    </span>
+                  </Link>
+                  <br />
+                  <br />
+                  <button className="sign-in-btn" onClick={this.handleSubmit}>
+                    Sign In
+                  </button>
+                  <br />
+                  <br />
+                </form>
+              </div>
+
+              <div className="demo-user">
+                <form>
+                  <input type="hidden" value="" />
+                  <input type="hidden" value="" />
+                  <button className="demo-user-btn" onClick={this.handleDemo}>
+                    Demo User
+                  </button>
+                </form>
+              </div>
           </div>
+
+
         </div>
       </div>
     );
@@ -118,3 +129,14 @@ export default Login
 
 
   //  <img className="sign-in-image" src={signInURL} />
+
+      // <div className="demo-user">
+      //     <form>
+      //       <input type="hidden" value={(this.state.username = "bqh5026")} />
+      //       <input type="hidden" value={(this.state.password = "password")} />
+      //       <button className="demo-user-btn" onClick={this.handleDemo}>
+      //         Demo User
+      //       </button>
+      //     </form>
+      //   </div>
+  
