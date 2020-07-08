00a39917f31a7fd49b91da4a543a46b1bae2191b
Ben Hsieh <bqh5026@gmail.com>
2020-07-08 00:17:27 -0400

    Add ability to add total portfolio values on account page

--- frontend/components/account/account.jsx
+++ frontend/components/account/account.jsx
@@ -92,7 +92,7 @@ export default ({ currentUser, logout }) => {
     const total = shares * quote.latest_price; 
     console.log(total); 
     axios
-      .post(`./portfolios/${currentUser.username}.json`, {Company: quote, Quantity: shares})
+      .post(`./portfolios/${currentUser.username}.json`, {Company: quote, Quantity: shares, Total: total})
       // .then(response => console.log(response))
       .then(document.querySelector(".buy-stock").textContent = "Bought")
       .then(document.querySelector(".buy-stock").disabled=true)
@@ -462,7 +462,7 @@ export default ({ currentUser, logout }) => {
                   <div className="total-portfolio-value">
                     $
                     {portfolioValue
-                      .map((a) => a.Company.latest_price)
+                      .map((a) => a.Total)
                       .reduce((a, b) => a + b, 0)
                       .toFixed(2)}
                   </div>
