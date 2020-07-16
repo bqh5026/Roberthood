d8b396968144886f6b2f892c85d3e48eeb47faee
Ben Hsieh <bqh5026@gmail.com>
2020-07-16 16:40:03 -0400

    Add ability to handle shares when stock exists in portfolio

--- frontend/components/account/account.jsx
+++ frontend/components/account/account.jsx
@@ -96,25 +96,15 @@ export default ({ currentUser, logout }) => {
         if (stock.Company.symbol === quote.symbol) {
           // debugger
           axios
-            .patch(
-              `./portfolios/${
-                currentUser.username/stock.firebaseID
-              }/Quantity.json`,
-              { Quantity: parseInt(stock.Quantity) + parseInt(shares) }
-            )
-            // .patch(
-            //   `./portfolios/${
-            //     currentUser.username
-            //   }.json`,
-            //   { Quantity: parseInt(stock.Quantity) + parseInt(shares) }
-            // )
+            .patch(`./portfolios/${currentUser.username}/${stock.firebaseID}.json`, {
+              Quantity: parseInt(stock.Quantity) + parseInt(shares),
+            })
+
             .then((document.querySelector(".buy-stock").textContent = "Bought"))
             .then((document.querySelector(".buy-stock").disabled = true));
             // console.log(JSON.stringify(currentUser.username/stock.firebaseID));
           return
-        } else {
-          console.log('False');
-        }
+        } 
     }
 
     if (shares >= 1) {
